e380293ded6b8db3f84e43465d1609dea93b56bc
chill-beer <juno87210@gmail.com>
2022-01-18 16:16:45 +0900

    fix: modified carousel indicator

--- src/components/indicator/index.tsx
+++ src/components/indicator/index.tsx
@@ -1,30 +1,23 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Stack } from "@mui/material";
 
-const Indicator = ({
-    length,
-    nowIdx,
-    setIdx,
-}: {
-    length: number
-    nowIdx: number
-    setIdx: any
-}) => {
+const Indicator = ({ length, nowIdx, setIdx }: { length: number; nowIdx: number; setIdx: any }) => {
     return (
-        <Stack direction="row" justifyContent="center" gap="12px">
+        <Stack direction="row" justifyContent="center" gap="2px">
             {new Array(length).fill(null).map((_: any, idx: number) => (
-                <Box
-                    className="buttonSelect"
-                    onClick={() => setIdx(idx)}
-                    sx={{
-                        width: nowIdx === idx ? '30px' : '8px',
-                        height: '8px',
-                        backgroundColor: nowIdx === idx ? '#0875f5' : '#cccccc',
-                        borderRadius: '5px',
-                    }}
-                />
+                <Box className="buttonSelect" key={idx} onClick={() => setIdx(idx)} sx={{ padding: nowIdx === idx ? "10px 0px" : "10px 5px", cursor: "pointer" }}>
+                    <Box
+                        className="buttonSelect"
+                        sx={{
+                            width: nowIdx === idx ? "30px" : "8px",
+                            height: "8px",
+                            backgroundColor: nowIdx === idx ? "#0875f5" : "#cccccc",
+                            borderRadius: "5px",
+                        }}
+                    ></Box>
+                </Box>
             ))}
         </Stack>
-    )
-}
+    );
+};
 
-export default Indicator
+export default Indicator;

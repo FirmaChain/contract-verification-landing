30104b41276230ab1074bcc542cdb2679240d284
chill-beer <juno87210@gmail.com>
2022-02-07 16:08:16 +0900

    fix: modified constants data

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,5 @@
-import { isBrowser } from 'react-device-detect'
-import { IKeyValue } from '../interface'
+import { isBrowser } from "react-device-detect";
+import { IKeyValue } from "../interface";
 import {
     IMG_SERVICE_DOCS,
     IMG_SERVICE_FAUCET,
@@ -15,78 +15,80 @@ import {
     M_IMG_SERVICE_FAUCET,
     M_IMG_SERVICE_DOCS,
     M_IMG_SERVICE_DONUE,
-} from './images'
-import { VERIFY_SERVICE } from './links'
+    M_IMG_VERIFICATION_V1,
+    M_IMG_VERIFICATION_V2,
+    IMG_VERIFICATION_V1,
+    IMG_VERIFICATION_V2,
+} from "./images";
+import { DONUE, FIRMACHAIN_DOCS, FIRMACHAIN_ECOSYSTEM, FIRMACHAIN_FAUCET, VERIFY_TOOL } from "./links";
+
+export const MENU_LIST: IKeyValue[] = [
+    { title: "VERIFY TOOL", goURL: VERIFY_TOOL },
+    { title: "API DOCS", goURL: FIRMACHAIN_DOCS },
+    { title: "ECOSYSTEM", goURL: FIRMACHAIN_ECOSYSTEM },
+];
 
 export const UTILIZATION_DATA: IKeyValue[] = [
     {
-        width: isBrowser ? '200px' : '147px',
-        imgURL: isBrowser
-            ? IMG_UTILIZATION_ECONTRACT
-            : M_IMG_UTILIZATION_ECONTRACT,
+        width: isBrowser ? "200px" : "147px",
+        imgURL: isBrowser ? IMG_UTILIZATION_ECONTRACT : M_IMG_UTILIZATION_ECONTRACT,
         data: {
-            title: 'E-Contract',
-            list: [
-                'Standard contract',
-                'Work contract',
-                'Salary contract',
-                'Sales contract',
-            ],
+            title: "E-Contract",
+            list: ["Standard contract", "Work contract", "Salary contract", "Sales contract"],
         },
     },
     {
-        width: isBrowser ? '212px' : '155px',
-        imgURL: isBrowser
-            ? IMG_UTILIZATION_ECOMMERCE
-            : M_IMG_UTILIZATION_ECOMMERCE,
+        width: isBrowser ? "212px" : "155px",
+        imgURL: isBrowser ? IMG_UTILIZATION_ECOMMERCE : M_IMG_UTILIZATION_ECOMMERCE,
         data: {
-            title: 'E-Commerce',
-            list: [
-                'Securities trading',
-                'Shopping mall',
-                'Second-hand deals',
-                'Auction',
-            ],
+            title: "E-Commerce",
+            list: ["Securities trading", "Shopping mall", "Second-hand deals", "Auction"],
         },
     },
     {
-        width: isBrowser ? '270px' : '197px',
+        width: isBrowser ? "270px" : "197px",
         imgURL: isBrowser ? IMG_UTILIZATION_SERVICE : M_IMG_UTILIZATION_SERVICE,
         data: {
-            title: 'Service',
-            list: [
-                'Document verification',
-                'Performance verification',
-                'History inquiry',
-                'Accounting ledger',
-            ],
+            title: "Service",
+            list: ["Document verification", "Performance verification", "History inquiry", "Accounting ledger"],
         },
     },
-]
+];
 
 export const SERVICE_LIST: IKeyValue[] = [
     {
         imgURL: isBrowser ? IMG_SERVICE_VERIFY : M_IMG_SERVICE_VERIFY,
-        goURL: VERIFY_SERVICE,
-        title: 'Verify',
-        desc: 'Verification service',
+        goURL: VERIFY_TOOL,
+        title: "Verify Tool",
+        desc: "Verification service",
     },
     {
         imgURL: isBrowser ? IMG_SERVICE_FAUCET : M_IMG_SERVICE_FAUCET,
-        goURL: '',
-        title: 'Faucet',
-        desc: 'Testnet faucet',
+        goURL: FIRMACHAIN_FAUCET,
+        title: "Faucet",
+        desc: "Testnet faucet",
     },
     {
         imgURL: isBrowser ? IMG_SERVICE_DOCS : M_IMG_SERVICE_DOCS,
-        goURL: '',
-        title: 'Verify docs',
-        desc: 'FIRMA VERIFY Guide',
+        goURL: FIRMACHAIN_DOCS,
+        title: "Verify docs",
+        desc: "FIRMA VERIFY Guide",
     },
     {
         imgURL: isBrowser ? IMG_SERVICE_DONUE : M_IMG_SERVICE_DONUE,
-        goURL: '',
-        title: 'Donue',
-        desc: 'E-Contract verification',
+        goURL: DONUE,
+        title: "Donue",
+        desc: "E-Contract verification",
     },
-]
+];
+
+export const VERIFICATION_SERVICE_LIST: IKeyValue[] = [
+    {
+        title: "Pahse 1: Module",
+        imgURL: isBrowser ? IMG_VERIFICATION_V1 : M_IMG_VERIFICATION_V1,
+    },
+    {
+        title: "Coming Soon",
+        imgURL: isBrowser ? IMG_VERIFICATION_V2 : M_IMG_VERIFICATION_V2,
+    },
+];
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,1 +1,7 @@
-export const VERIFY_SERVICE = 'http://verify-tool.firmachain.io'
+export const VERIFY_TOOL = process.env.REACT_APP_VERIFY_TOOL_HOST;
+export const FIRMACHAIN_ECOSYSTEM = "https://firmachain.org/ecosystem/";
+export const FIRMACHAIN_DOCS = "https://docs.firmachain.org/master/developer/modules/contract";
+export const FIRMACHAIN_FAUCET = "https://faucet-testnet.firmachain.dev/";
+export const DONUE = "https://donue.co.kr/";
+export const INFO_MAIL = "info@firmachain.org";
+export const CONTACT_MAIL = "contact@firmachain.org";

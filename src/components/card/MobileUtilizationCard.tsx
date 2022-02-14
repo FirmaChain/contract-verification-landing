fb0ca3740d2b118ca92ef07ccb0e0939e1e0ca2e
chill-beer <juno87210@gmail.com>
2022-02-14 13:10:33 +0900

    fix: modified hover effect for utilization card

--- src/components/card/MobileUtilizationCard.tsx
+++ src/components/card/MobileUtilizationCard.tsx
@@ -1,85 +1,87 @@
-import { IKeyValue } from '../../interface'
-import { Divider, Stack, Typography } from '@mui/material'
-import { Metropolis } from '../../constants/theme'
+import { IKeyValue } from "../../interface";
+import { Divider, Stack, Typography } from "@mui/material";
+import { Metropolis } from "../../constants/theme";
+import { useState } from "react";
 
 const MobileUtilizationCard = ({ data }: IKeyValue) => {
+    const [hover, setHover] = useState(false);
+
     return (
-        <Stack sx={{ position: 'relative' }} id={''}>
-            <img src={data.imgURL} alt="" style={{ width: '278px' }} />
+        <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <Stack sx={{ position: "relative", overflow: "hidden", borderRadius: "12px" }} id={""}>
+                <img src={data.imgURL} alt="" style={{ width: "278px" }} draggable={false} />
 
-            <Stack
-                className="hoverShow"
-                sx={{
-                    // Box
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                    position: 'absolute',
-                    borderRadius: '8px',
-                    opacity: 0,
-                    userSelect: 'none',
-                    padding: '0 28px',
-                    boxSizing: 'border-box',
-                    ':hover': {
-                        opacity: 1,
-                    },
-
-                    // content
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#fff',
-                }}
-            >
                 <Stack
-                    direction="column"
-                    alignItems="center"
-                    sx={{ width: data.width }}
+                    className="hoverShow"
+                    sx={{
+                        // Box
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "rgba(0, 0, 0, 0.8)",
+                        position: "absolute",
+                        top: hover ? 0 : 460,
+                        opacity: hover ? 1 : 0,
+                        borderRadius: "8px",
+                        userSelect: "none",
+                        padding: "0 28px",
+                        boxSizing: "border-box",
+
+                        // content
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#fff",
+                    }}
                 >
+                    <Stack className={hover ? "hoverShowDesc" : "hoverHiddenDesc"} direction="column" alignItems="center" sx={{ width: data.width, paddingTop: "50px", opacity: hover ? 1 : 0 }}>
+                        <Divider
+                            light={true}
+                            orientation="horizontal"
+                            component="div"
+                            sx={{
+                                width: "100%",
+                                color: "#ffffff80",
+                                backgroundColor: "#ffffff80",
+                                mb: "20px",
+                            }}
+                        />
+
+                        <Stack sx={{ width: "100%" }}>
+                            {data.data.list.map((one: string, idx: number) => (
+                                <Typography
+                                    key={idx}
+                                    sx={{
+                                        fontFamily: Metropolis,
+                                        fontSize: "16px",
+                                        fontWeight: 500,
+                                        lineHeight: 2.11,
+                                        letterSpacing: "-0.27px",
+                                    }}
+                                >
+                                    {`• ${one}`}
+                                </Typography>
+                            ))}
+                        </Stack>
+                    </Stack>
+                </Stack>
+                <Stack className="hoverShow" sx={{ width: "100%", height: "100%", position: "absolute", top: hover ? 60 : 270 }}>
                     <Typography
+                        className="hoverShow"
                         sx={{
                             fontFamily: Metropolis,
-                            fontSize: '24px',
-                            fontWeight: 600,
-                            lineHeight: 0.75,
-                            letterSpacing: '-0.36px',
-                            pb: '20px',
+                            fontSize: hover ? "26px" : "22px",
+                            fontWeight: hover ? 600 : 500,
+                            textAlign: "center",
+                            letterSpacing: "-1.8px",
+                            color: "#fff",
+                            pb: "20px",
                         }}
                     >
                         {data.data.title}
                     </Typography>
-
-                    <Divider
-                        light={true}
-                        orientation="horizontal"
-                        component="div"
-                        sx={{
-                            width: '100%',
-                            color: '#fff',
-                            backgroundColor: '#fff',
-                            mb: '20px',
-                        }}
-                    />
-
-                    <Stack sx={{ width: '100%' }}>
-                        {data.data.list.map((one: string, idx: number) => (
-                            <Typography
-                                key={one}
-                                sx={{
-                                    fontFamily: Metropolis,
-                                    fontSize: '16px',
-                                    fontWeight: 500,
-                                    lineHeight: 2.11,
-                                    letterSpacing: '-0.27px',
-                                }}
-                            >
-                                - {one}
-                            </Typography>
-                        ))}
-                    </Stack>
                 </Stack>
             </Stack>
-        </Stack>
-    )
-}
+        </div>
+    );
+};
 
-export default MobileUtilizationCard
+export default MobileUtilizationCard;
--- src/components/card/UtilizationCard.tsx
+++ src/components/card/UtilizationCard.tsx
@@ -1,82 +1,93 @@
-import { IKeyValue } from '../../interface'
-import { Divider, Stack, Typography } from '@mui/material'
-import { Metropolis } from '../../constants/theme'
+import { IKeyValue } from "../../interface";
+import { Divider, Stack, Typography } from "@mui/material";
+import { Metropolis } from "../../constants/theme";
+import { useState } from "react";
 
 const UtilizationCard = ({ data }: IKeyValue) => {
-    return (
-        <Stack sx={{ position: 'relative' }} id={data.data.title}>
-            <img src={data.imgURL} alt="" style={{ maxWidth: '380px' }} />
-            <Stack
-                className="hoverShow"
-                sx={{
-                    // Box
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                    position: 'absolute',
-                    borderRadius: '12px',
-                    opacity: 0,
-                    userSelect: 'none',
-                    padding: '0 50px',
-                    boxSizing: 'border-box',
-                    ':hover': {
-                        opacity: 1,
-                    },
+    const [hover, setHover] = useState(false);
 
-                    // content
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#fff',
-                }}
-            >
+    return (
+        <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <Stack sx={{ position: "relative", overflow: "hidden", borderRadius: "12px" }} id={data.data.title}>
+                <img src={data.imgURL} alt="" style={{ maxWidth: "380px" }} draggable={false} />
                 <Stack
+                    className="hoverShow"
                     sx={{
-                        width: data.width,
-                        alignItems: 'center',
-                        gap: '20px',
+                        // Box
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "rgba(0, 0, 0, 0.8)",
+                        position: "absolute",
+                        top: hover ? 0 : 460,
+                        opacity: hover ? 1 : 0,
+                        borderRadius: "12px",
+                        userSelect: "none",
+                        padding: "0 50px",
+                        boxSizing: "border-box",
+
+                        // content
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#fff",
                     }}
                 >
+                    <Stack
+                        className={hover ? "hoverShowDesc" : "hoverHiddenDesc"}
+                        sx={{
+                            width: data.width,
+                            alignItems: "center",
+                            gap: "20px",
+                            paddingTop: "80px",
+                            opacity: hover ? 1 : 0,
+                        }}
+                    >
+                        <Divider
+                            light={true}
+                            orientation="horizontal"
+                            component="div"
+                            sx={{
+                                width: "100%",
+                                color: "#ffffff80",
+                                backgroundColor: "#ffffff80",
+                            }}
+                        />
+
+                        <Stack sx={{ width: "100%" }}>
+                            {data.data.list.map((one: string, idx: number) => (
+                                <Typography
+                                    key={idx}
+                                    sx={{
+                                        fontFamily: Metropolis,
+                                        fontSize: "22px",
+                                        fontWeight: 400,
+                                        lineHeight: 2,
+                                        letterSpacing: "-0.33px",
+                                    }}
+                                >
+                                    {`• ${one}`}
+                                </Typography>
+                            ))}
+                        </Stack>
+                    </Stack>
+                </Stack>
+                <Stack className="hoverShow" sx={{ width: "100%", height: "100%", position: "absolute", top: hover ? 100 : 375 }}>
                     <Typography
+                        className="hoverShow"
                         sx={{
                             fontFamily: Metropolis,
-                            fontSize: '28px',
-                            fontWeight: 500,
-                            letterSpacing: '-0.42px',
+                            fontSize: hover ? "32px" : "28px",
+                            fontWeight: hover ? 600 : 500,
+                            textAlign: "center",
+                            color: "#fff",
+                            letterSpacing: "-1.8px",
                         }}
                     >
                         {data.data.title}
                     </Typography>
-
-                    <Divider
-                        light={true}
-                        orientation="horizontal"
-                        component="div"
-                        sx={{
-                            width: '100%',
-                            color: '#fff',
-                            backgroundColor: '#fff',
-                        }}
-                    />
-
-                    <Stack sx={{ width: '100%' }}>
-                        {data.data.list.map((one: string, idx: number) => (
-                            <Typography
-                                sx={{
-                                    fontFamily: Metropolis,
-                                    fontSize: '22px',
-                                    fontWeight: 400,
-                                    lineHeight: 2,
-                                    letterSpacing: '-0.33px',
-                                }}
-                            >
-                                - {one}
-                            </Typography>
-                        ))}
-                    </Stack>
                 </Stack>
             </Stack>
-        </Stack>
-    )
-}
+        </div>
+    );
+};
 
-export default UtilizationCard
+export default UtilizationCard;

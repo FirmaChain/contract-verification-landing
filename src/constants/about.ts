0d595958a150d5f81e023501edcdd6baae18316f
chill-beer <juno87210@gmail.com>
2023-04-04 17:44:49 +0900

    update: change image and link

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -3,7 +3,7 @@ export const ABOUT_SECTION = "ABOUT";
 export const ABOUT_TITLE = "Build\na higher level\nof trust.";
 export const ABOUT_TITLE_MOBILE = "Build a higher\nlevel of trust.";
 
-export const ABOUT_DESCRIPTION = "All recorded data on the blockchain network are\ntransparently disclosed and cannot be modified.\n Using our verification service, build your own service.";
+export const ABOUT_DESCRIPTION = "All recorded data on the blockchain network are\ntransparently disclosed and cannot be modified.\nUsing our verification service, build your own service.";
 export const ABOUT_DESCRIPTION_MOBILE = "All recorded data on the blockchain\nnetwork are transparently disclosed\nand cannot be modified. \nUsing our verification service,\nbuild your own service.";
 
 export const ABOUT_API_TITLE = "Open API\nfor Easy\nImplementation.";
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,6 +2,6 @@ export const VERIFY_TOOL = process.env.REACT_APP_VERIFY_TOOL_HOST;
 export const FIRMACHAIN_ECOSYSTEM = "https://firmachain.org/ecosystem/";
 export const FIRMACHAIN_DOCS = "https://docs.firmachain.org/master/developer/modules/contract";
 export const FIRMACHAIN_FAUCET = "https://faucet-testnet.firmachain.dev/";
-export const DONUE = "https://donue.co.kr/";
+export const DONUE = "https://donue.co.kr/contract-verification/";
 export const INFO_MAIL = "info@firmachain.org";
 export const CONTACT_MAIL = "contact@firmachain.org";
